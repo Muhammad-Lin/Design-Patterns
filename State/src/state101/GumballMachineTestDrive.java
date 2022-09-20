69d96d8793932e573dcafc71221051e3457e8c9d
yoga <3416877267@qq.com>
2022-09-20 13:54:36 +0800

    状态机101代码实现

---
+++ State/src/state101/GumballMachineTestDrive.java
@@ -0,0 +1,38 @@
+package state101;
+
+/**
+ * @name: GumballMachineTestDrive
+ * @author: yoga
+ * @create: 2022-09-19 16:52
+ **/
+public class GumballMachineTestDrive {
+    public static void main(String[] args){
+        GumballMachine gumballMachine = new GumballMachine(5);
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.ejectQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.ejectQuarter();
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+    }
+}
